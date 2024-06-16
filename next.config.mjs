d925e234dc18b914f4d096a5cef92c3ffca504eb
Gustavo Dos Santos <simondossantos@gmail.com>
2024-06-15 22:54:42 -0300

    config nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+            key: "Access-Control-Allow-Methods",
+            value: "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   swcMinify: true,
   images: {
     remotePatterns: [
